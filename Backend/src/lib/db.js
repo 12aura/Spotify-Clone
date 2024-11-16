bf727cd9746718f0e48a52f5ae6cce5d3248b0de
12aura <auraannu@gmail.com>
2024-11-16 09:33:01 +0530

    Done Setup Frontend and Backend

---
+++ Backend/src/lib/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+// Correct way to import in CommonJS (no need for .js extension)
+
+
+export const connectDB = async () => {
+    try{
+        const conn = await mongoose.connect(process.env.MONGODB_URI);
+        console.log(`Connected to MongoDB ${conn.connection.host}`);
+    }catch(error){
+        console.log("Failed to connect to MongoDB", error);
+        process.exit(1);// 1 is failure 0 is success message
+    }
+    
+}
