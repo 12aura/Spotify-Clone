db74415427b111652c1f3b25ebf81f466208900b
12aura <auraannu@gmail.com>
2024-11-16 09:57:30 +0530

    Added code to check to logIn and SignUp

--- Backend/src/routes/admin.route.js
+++ Backend/src/routes/admin.route.js
@@ -1,10 +1,10 @@
 import { Router } from 'express';
+//import { getAdmin } from "../controller/admin.controller.js";
+import { protectRoute, requireAdmin } from '../middleware/auth.middleware.js';
+
 
 const router = Router();
 
-router.get('/', (req, res) =>
-{
-    res.send('Admin route with get method');
-});
+router.get('/', protectRoute, requireAdmin, createSong);
 
 export default router;
