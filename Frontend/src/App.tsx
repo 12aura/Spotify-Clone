a22ff54affde57f37d3f2b3abd0cd264f1b5211e
12aura <auraannu@gmail.com>
2024-11-16 19:07:11 +0530

    Added different types in a single folder

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,22 +1,38 @@
-import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/clerk-react";
-import { Button } from "./components/ui/button";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home/HomePage.tsx";
+import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage.tsx";
+import { Home } from "lucide-react";
+import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
+import MainLayout from "./layout/MainLayout";
+import ChatPage from "./pages/chat/ChatPage";
+import AlbumPage from "./pages/album/AlbumPage";
+import AdminPage from "./pages/admin/AdminPage";
+
+import { Toaster } from "react-hot-toast";
+import NotFoundPage from "./pages/404/NotFoundPage";
 
 function App() {
+	return (
+		<>
+			<Routes>
+				<Route
+					path='/sso-callback'
+					element={<AuthenticateWithRedirectCallback signUpForceRedirectUrl={"/auth-callback"} />}
+				/>
+        <Route path="/" element={<HomePage/>}/> 
+				<Route path='/auth-callback' element={<AuthCallbackPage />} />
+				<Route path='/admin' element={<AdminPage />} />
 
-  return (
-    <>
-    <header>
-      <SignedOut>
-        <SignInButton>
-          <><Button> Sign In</Button></>
-        </SignInButton >
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </header>
-    </>
-  );
+				<Route element={<MainLayout />}>
+					<Route path='/' element={<HomePage />} />
+					<Route path='/chat' element={<ChatPage />} />
+					<Route path='/albums/:albumId' element={<AlbumPage />} />
+					<Route path='*' element={<NotFoundPage />} />
+				</Route>
+			</Routes>
+			<Toaster />
+		</>
+	);
 }
 
-export default App
+export default App;
