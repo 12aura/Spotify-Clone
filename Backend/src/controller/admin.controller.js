585901b46b5a447530eb7e8aa6e583d626d38c65
12aura <auraannu@gmail.com>
2024-11-16 10:28:27 +0530

    Added cloudinary to easily upload and store files

--- Backend/src/controller/admin.controller.js
+++ Backend/src/controller/admin.controller.js
@@ -1,31 +1,3 @@
-import { Router } from "express";
-import { User } from "../models/user.model.js";
-
-const router = Router();
-
-router.post("/callback", async(req, res) => 
-{
-    try{
-        const {id, firstName, lastName, imageUrl}= req.body;
-
-        const user = await User.findOne({clerkId: id});
-
-        if(!user)
-        {
-            await User.create(
-                {
-                    clerkId: id,
-                    fullName: `${firstName} ${lastName}`,
-                    imageUrl
-                }
-            )
-        }
-        res.status(200).json({success: true})
-    }catch(error)
-    {
-        console.log("Error in auth callback", error);
-        res.status(500).json({message: "Internal server error", error})
-    }
-});
-
-export default router;
+export const createSong = (req, res) => {
+    res.send("Admin");
+};
